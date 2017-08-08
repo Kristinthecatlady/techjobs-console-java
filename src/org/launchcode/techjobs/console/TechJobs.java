b9a8990d590977264b151a141283ddd424dd8ff4
Kristin Alarcon <kristin.m.alarcon@gmail.com>
2017-08-07 20:01:48 -0700

    Got search working to start

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobs.console;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -110,7 +111,17 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0){
+            System.out.println("No positions available with that criteria, sorry!");
+        }
+        else{
+            for (int i = 0 ; i < someJobs.size(); i++){
+                System.out.println("\n1*****\n");
+                for (Map.Entry<String, String> part : someJobs.get(i).entrySet()){
+                    System.out.println(part.getKey() + ":  ".concat(part.getValue()));
+                }
+            }
+            System.out.println("\n*****");
+        }
     }
-}
+    }
